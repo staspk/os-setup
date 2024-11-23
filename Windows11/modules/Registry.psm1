37c3c92b791dc39284f6a4d893c094e7c3c8d34a
Stanislav Kozubenko <staspk@gmail.com>
2024-11-23 03:47:06 -0800

    Registry Functions Made Safe

--- Windows11/modules/Registry.psm1
+++ Windows11/modules/Registry.psm1
@@ -1,73 +1,88 @@
-# Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion		"Registry::" may work to save it all?
-$EXPLORER_ADVANCED = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-$POLICIES_MICROSOFT = "HKLM:\SOFTWARE\Policies\Microsoft"
-$TOUCHED_KEYS = $EXPLORER_ADVANCED, $POLICIES_MICROSOFT
-
-$BACKUP_FOLDER = ".\registry-backup"
-$BACKUP_FILE = "$BACKUP_FOLDER\registry-backup.reg"
-
-function BackupRegistry {
-	mkdir -Force $BACKUP_FOLDER
-	foreach($key in $TOUCHED_KEYS) {
-		$key = $key.Replace(":","")
-		reg export $key $BACKUP_FOLDER\$key 
-	}
-	Write-Host "Backup of Registry has been exported to $BACKUP_FILE"
-}
-
-function RestoreRegistry($from = $BACKUP_FILE) {	#Unfinished. Wanna use functionality, finish it
-	reg import $from
-	Write-Host "Registry has been updated from backup file: $from"
-}
-
 function NewRegistryKey($path, $name) {
-	New-Item -Path $path -Name $name
+	if (-not (Test-Path $path)) {
+		New-Item -Path $path -Name $name
+	}
+	else { Write-Host "New Registry Function used, but registry key already exists. Path: $path. Name: $name" }
 }
 
-function NewDword($path, $name, $value) {
-	New-ItemProperty -Path $path -Name $name -PropertyType "DWORD" -Value $value  | out-null
-}
+function RegistryPropertyEditOrAdd($path, $name, $value, $propertyType = "DWORD") {
+	$exists =  If ((Get-Item $path).property -match $propName) {"true"}
 
-function ChangeRegistryValue($path, $name, $value) {
-	Set-ItemProperty -Path $path -Name $name -Value $value
-}
-
-
-function FileExplorerDefaultOpenTo($default = 3) {	# 1=ThisPC, 2=QuickAccess, 3=Downloads
-	NewDword $EXPLORER_ADVANCED "LaunchTo" $default
-	Write-Host "FILE EXPLORER: will now default-open to Downloads"
-}
-
-function VisibleFileExtensions($hideFileExt = 0) {	#hideFileExt=1 for opposite effect
-	ChangeRegistryValue $EXPLORER_ADVANCED "HideFileExt" $hideFileExt
-	Write-Host "FILE EXPLORER: File's format/extension will now be visible in file names."
-}
-
-function VisibleHiddenFiles($hidden = 1) {	#hidden=2 for opposite effect
-	ChangeRegistryValue $EXPLORER_ADVANCED "Hidden" $hidden
-	Write-Host "FILE EXPLORER: Hidden files & folders will now be visible."
+	if($exists) {
+		Set-ItemProperty -Path $path -Name $name -Value $value  }
+	else {
+		New-ItemProperty -Path $path -Name $name -PropertyType $propertyType -Value $value  | Out-Null  }
 }
 
 
+enum FileExplorerLaunchTo {  ThisPC = 1; QuickAccess = 2; Downloads = 3  }
+function FileExplorerDefaultOpenTo([FileExplorerLaunchTo]$launchTo = [FileExplorerLaunchTo]::Downloads) { 
+	[int]$enumAsInt = [Enum]::Parse([FileExplorerLaunchTo], $launchTo)
 
-function TaskBarAlignment($alignment = 0) {
-	ChangeRegistryValue $EXPLORER_ADVANCED "TaskbarAl" $alignment
-	Write-Host "TASKBAR: Alignment set to left."
+	RegistryPropertyEditOrAdd $EXPLORER_ADVANCED "LaunchTo" $enumAsInt
+
+	Write-Host "FILE EXPLORER: will now default-open to: $launchTo"
+}
+
+function ShowRecentInQuickAccess( [ValidateRange(0, 1)] $bool = 0 ) { 
+	$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer"
+	$propName = "ShowRecent"
+
+	RegistryPropertyEditOrAdd $path $propName $bool
+
+	if ($bool -eq 0) {	Write-Host "FILE EXPLORER: Will stop automatically populating Quick Access with Recently Used Directories/Files" -ForegroundColor Green	 }
+	elseif ($bool -eq 1) {	Write-Host "FILE EXPLORER: Quick Access will now be automatically populated with Recently Used Files [preset win11 behavior]" -ForegroundColor Green  }
+}
+
+function VisibleFileExtensions( [ValidateRange(0, 1)] $bool = 0) {
+	$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+	$propName = "HideFileExt"
+
+	RegistryPropertyEditOrAdd $path $propName $bool
+
+	Write-Host "FILE EXPLORER: File's format/extension (in)visibility boolean status updated to: $bool" -ForegroundColor Green
+}
+
+function VisibleHiddenFiles( [ValidateRange(0, 1)] $bool = 1) {
+	$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+	$propName = "Hidden"
+
+	RegistryPropertyEditOrAdd $path $propName $bool
+
+	Write-Host "FILE EXPLORER: Hidden files & folders boolean status set to: $bool." -ForegroundColor Green
+}
+
+
+function TaskBarAlignment([ValidateRange(0, 1)] $alignment = 0) {
+	$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+	$propName = "TaskbarAl"
+
+	RegistryPropertyEditOrAdd $path $propName $alignment
+
+	if ($alignment -eq 0) {  Write-Host "TASKBAR: Alignment set to left." -ForegroundColor Green  }
+	elseif ($alignment -eq 1) {  Write-Host "TASKBAR: Alignment set to center." -ForegroundColor Green  }
+	
 }
 
 function TaskBarRemoveTaskView {
-	ChangeRegistryValue $EXPLORER_ADVANCED "ShowTaskViewButton" 0
-}
+	$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+	$propName = "ShowTaskViewButton"
 
+	ChangeRegistryValue $path $propName 0
+
+	Write-Host "TASKBAR: Task View Removed"
+}
 
 function DisableWidgets {
-	$KEY = "Dsh"
-	NewRegistryKey $POLICIES_MICROSOFT $KEY
-	NewDword "$POLICIES_MICROSOFT\$KEY" "AllowNewsAndInterests" 0 
-	Write-Host "Disabled Widgets. Please restart Computer to finalize changes."
+	$path = "HKLM:\SOFTWARE\Policies\Microsoft"
+	$key = "DSH";  $propName = "AllowNewsAndInterests"
+
+	NewRegistryKey $path $key
+	RegistryPropertyEditOrAdd "$path\$key" $propName 0
+	
+	Write-Host "Disabled Widgets. Please restart Computer to finalize changes." -ForegroundColor Green
 }
 
 
-
-Export-ModuleMember -Function BackupRegistry, RestoreRegistry, VisibleFileExtensions, VisibleHiddenFiles, FileExplorerDefaultOpenTo, TaskBarAlignment, TaskBarRemoveTaskView, DisableWidgets
+Export-ModuleMember -Function FileExplorerDefaultOpenTo, ShowRecentInQuickAccess, VisibleFileExtensions, VisibleHiddenFiles, TaskBarAlignment, TaskBarRemoveTaskView, DisableWidgets
 
