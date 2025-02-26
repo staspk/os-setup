450c2d9a5fd904a268115087e7587eaab1c78050
Stanislav Kozubenko <staspk@gmail.com>
2025-02-26 01:57:46 -0800

    Powershell/VsCode Settings Backup

--- Windows11/.powershell/Powershell/Kozubenko.IO.psm1
+++ Windows11/.powershell/Powershell/Kozubenko.IO.psm1
@@ -4,11 +4,50 @@ class KozubenkoIO {
         return [FunctionRegistry]::new(
             "Kozubenko.IO",
             @(
+                "AddToEnvPath(`$newPathItem)        -->  add to Windows Env PATH",
                 "DisplayFolderSizes()              -->  lists folders in current directory with their sizes (not on disk)",
                 "ClearFolder(`$folder = '.\')       -->  recursively deletes contents of directory", 
                 "LockFolder(`$folder)               -->  remove write access rules for 'Everyone'"
             ));
     }
+}
+
+# Adds to Windows PATH
+function AddToEnvPath($newPathItem) {
+    $windowsPath = [Environment]::GetEnvironmentVariable("PATH", "User")
+
+    Write-Host $windowsPath
+    
+    $pathArray = $windowsPath.Split(";") | ForEach-Object { $_.Trim() }
+
+    foreach ($item in $pathArray) {
+        $item
+    }
+
+    if ($pathArray -contains $newPathItem) {  WriteRed "The path '$newPathItem' is already in your PATH.";  RETURN;  }
+
+    $newPath = ""
+    foreach ($pathItem in $pathArray) {  $newPath += $pathItem + ";"  }
+    $newPath += $newPathItem;
+
+    Write-Host $newPath
+
+    [System.Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
+}
+
+function RemoveFromEnvPath($pathItemToRemove) {     # example $pathItemToRemove: %USERPROFILE%\AppData\Local\Microsoft\WindowsApps
+    $currentPath = [System.Environment]::GetEnvironmentVariable("PATH", "User")
+    $pathArray = $currentPath.Split(";")
+
+    $newPath = ""
+    for ($i = 0; $i -lt $pathArray.Count; $i++) {
+        if ($pathArray[$i] -ne $pathItemToRemove -and $i -ne $($pathArray.Count - 1)) {
+            $newPath += "$pathArray[$i];"
+        }
+    }
+    $newPath += $pathArray[$($pathArray.Count - 1)]
+
+    [System.Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
 }
 
 function DisplayFolderSizes {
