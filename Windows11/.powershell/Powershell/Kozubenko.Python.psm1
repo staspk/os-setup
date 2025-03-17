55075e6c59a0ece5c3cfac413d17c90d1984255c
Stanislav Kozubenko <staspk@gmail.com>
2025-03-16 18:57:28 -0700

    Powershell/VsCode Settings Backup

--- Windows11/.powershell/Powershell/Kozubenko.Python.psm1
+++ Windows11/.powershell/Powershell/Kozubenko.Python.psm1
@@ -4,7 +4,7 @@ class KozubenkoPython {
         return [FunctionRegistry]::new(
             "Kozubenko.Python",
             @(
-                "SetupBasicPythonEnvironment()         -->   setups .venv alongside basic necessities"
+                "SetupBasicPythonProject()             -->   setups .venv alongside basic necessities"
                 "CreateVenvEnvironment()               -->   py -m venv .venv",
                 "Activate()                            -->   .\.venv\Scripts\Activate.ps1",
                 "venvFreeze()                          -->   pip freeze > requirements.txt",
@@ -19,7 +19,7 @@ $global:venvActive = $false
 
 $BOILERPLATE_PYTHON_PROJECT = "$profile\..\boilerplate\python_minimum_vscode_setup"
 
-function SetupBasicPythonEnvironment($path = $PWD.Path) {
+function SetupBasicPythonProject($path = $PWD.Path) {
     if (-not(TestPathSilently $path)) {
         WriteDarkRed "`please give valid `$path"
         RETURN;
@@ -32,8 +32,6 @@ function SetupBasicPythonEnvironment($path = $PWD.Path) {
     python.exe -m pip install --upgrade pip
 
     Copy-Item -Path "$BOILERPLATE_PYTHON_PROJECT\*" -Destination $path -Recurse
-
-    Set-Content -Path "$BOILERPLATE_PYTHON_PROJECT\.vscode\launch.json" -Value $(_VsCode_Python_Launch_Json $path)
 
     Clear-Host
 }
@@ -71,22 +69,4 @@ function venvInstallRequirements {
 
 function KillPythonProcesses {
     Get-Process -Name python | Stop-Process -Force
-}
-
-
-function _VsCode_Python_Launch_Json($project_root) {
-    $project_root = $project_root.Replace('\', '/')
-
-    return "{
-        `"configurations`": [
-            {
-                `"name`": `"Python: Debug main.py`",
-                `"type`": `"debugpy`",
-                `"python`": `"$project_root/.venv/Scripts/python.exe`",
-                `"request`": `"launch`",
-                `"program`": `"`${workspaceFolder}/main.py`",
-                `"justMyCode`": false
-            }
-        ]
-    }"
 }
