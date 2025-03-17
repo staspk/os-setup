1e4805cd5894711a73dbdf4278e49d4a21f142d1
Stanislav Kozubenko <staspk@gmail.com>
2025-03-16 23:22:02 -0700

    VsCode Settings Backup

--- saveWindowsSettingsAndGithubPush.ps1
+++ saveWindowsSettingsAndGithubPush.ps1
@@ -1,15 +1,13 @@
-using module ".\Windows11\modules\Powershell.psm1"
 using module ".\Windows11\modules\VsCode.psm1"
 
 param(
     $commitMessage
 )
 
-$configurer = [PowershellConfigurer]::new().SaveProfileFilesToScriptPackage()
 $VsCode = [VsCode]::new().BackupVsCode()
 
 if($commitMessage -eq $null) {
-    $commitMessage = "Powershell/VsCode Settings Backup"
+    $commitMessage = "VsCode Settings Backup"
 }
 
 
