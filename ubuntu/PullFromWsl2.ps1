7df0754cfa3efc3e966dab25289030a48b2ef598
Stanislav Kozubenko <staspk@gmail.com>
2025-03-22 19:03:59 -0700

    submodule redo

--- ubuntu/PullFromWsl2.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    $ubuntu_home              # e.g: Microsoft.PowerShell.Core\FileSystem::\\wsl$\Ubuntu-24.04\home\stan
-    $os_setup_ubuntu          # absolute path of: os-setup/ubuntu
-)
-
-PrinctCyan $ubuntu_home
-
