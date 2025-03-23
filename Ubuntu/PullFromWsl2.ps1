6bc25f9e5ae89dd12568e0ca17950700b9318318
Stanislav Kozubenko <staspk@gmail.com>
2025-03-22 18:45:48 -0700

    experimental wsl2 script started

---
+++ Ubuntu/PullFromWsl2.ps1
@@ -0,0 +1,7 @@
+param(
+    $ubuntu_home              # e.g: Microsoft.PowerShell.Core\FileSystem::\\wsl$\Ubuntu-24.04\home\stan
+    $os_setup_ubuntu          # absolute path of: os-setup/ubuntu
+)
+
+PrinctCyan $ubuntu_home
+
