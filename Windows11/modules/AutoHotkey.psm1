f03f39248f28e0821c85415ac734c15b9d984f2c
Stanislav Kozubenko <staspk@gmail.com>
2025-06-28 18:19:05 -0700

    Automatic Settings Backup

--- Windows11/modules/AutoHotkey.psm1
+++ Windows11/modules/AutoHotkey.psm1
@@ -43,5 +43,8 @@ function BackupAutoHotkey() {
     }
     catch {  PrintRed "BackupAutoHotkey(): Failure! Reason: $($_.Exception.Message)"; RETURN; }
 
-    PrintGreen "BackupAutoHotkey($toBackup): Success! `$backupDest: $backupDest"
+    PrintGreen "BackupAutoHotkey(" $false
+    PrintDarkGreen $toBackup $false;
+    PrintGreen "): Success! `$backupDest: " $false
+    PrintDarkGreen $backupDest
 }
